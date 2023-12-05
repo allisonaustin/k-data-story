4bedc219d756184196d9539d4b5aa1e614d2a25e
yuchiatw <claire.hsinchu@gmail.com>
2023-12-05 14:33:01 -0800

    heatmap

--- vue-project/src/components/HeatmapView.vue
+++ vue-project/src/components/HeatmapView.vue
@@ -3,49 +3,73 @@ import * as d3 from 'd3';
 import { isEmpty, debounce } from 'lodash';
 import { ComponentSize, Margin } from '../types';
 
-const data = await d3.csv('../../data/k_data_processed.csv');
+const temp_data_err = await d3.csv('../../data/temp-data-l07.csv');
+const temp_data_nor = await d3.csv('../../data/temp-data-m05.csv');
+
 const rack_err = 'l07'
-const bp_err = '0'
-const sb_err = '6'
-
-
-let tempData = data.map(d => {
+const rack_nor = 'm05'
+const bp_err = 0
+const sb_err = 6
+const errTime = new Date("2019-05-28 14:52:00")
+let temp_table_err_bf = []
+let temp_table_err_af = []
+temp_data_err.forEach((d, i) => {
     let Obj = {
         check_time: d3.timeParse('%Y-%m-%d %H:%M:%S')(d.check_time),
-        cpu0: +d[`bp${bp_err}_sb${sb_err}_cpu_0_temp`],
-        cpu1: +d[`bp${bp_err}_sb${sb_err}_cpu_1_temp`],
-        cpu2: +d[`bp${bp_err}_sb${sb_err}_cpu_2_temp`],
-        cpu3: +d[`bp${bp_err}_sb${sb_err}_cpu_3_temp`],
-
-        rack: d.rack
+        time_idx: i,
+        rack: rack_err
     }
-    return Obj
-})
-let tempDataErr = []
-let tempDataNor = []
-Object.values(tempData).forEach(d => {
-    if (d.rack == rack_err) {
-        tempDataErr.push(d)
+    Object.keys(d).forEach((c) => {
+        if (String(c).includes('temp')) {
+            Obj[c] = +d[c]
+        }
+    })
+    if (Obj['check_time'] < errTime){
+        temp_table_err_bf.push(Obj)
     }
     else {
-        tempDataNor.push(d)
+        temp_table_err_af.push(Obj)
     }
+})
+let temp_table_nor_bf = []
+let temp_table_nor_af = []
+temp_data_nor.forEach((d, i) => {
+    let Obj = {
+        check_time: d3.timeParse('%Y-%m-%d %H:%M:%S')(d.check_time),
+        time_idx: i,
+        rack: rack_nor
+    }
+    Object.keys(d).forEach((c) => {
+        if (String(c).includes('temp')) {
+            Obj[c] = +d[c]
+        }
+
+    })
+
+    if (Obj['check_time'] < errTime){
+        temp_table_nor_bf.push(Obj)
+    }
+    else {
+        temp_table_nor_af.push(Obj)
+    }
+    
 })
 export default {
     data() {
         return {
-            // size: { width: 1000, height: 500 } as ComponentSize,
-            size: { width: 0, height: 0 } as ComponentSize,
-            margin: {left: 40, right: 25, top: 20, bottom: 20} as Margin,
-        }
-    }, 
+            size: { width: 800, height: 500 } as ComponentSize,
+            svgSize: { width: 760, height: 100 } as ComponentSize,
+            svgSizeBf: { width: 245, height: 100 } as ComponentSize,
+            svgSizeAf: { width: 720, height: 100 } as ComponentSize,
+            margin: {left: 40, right: 10, top:0, bottom: 0} as Margin,
+        } }, 
     computed: {
         rerender() {
-            return (!isEmpty(data)) && this.size
+            return (!isEmpty(temp_data_err)) && this.size
         }
     },
     created() {
-        if (isEmpty(data)) return;
+        if (isEmpty(temp_data_err)) return;
         this.initChart()
     },
     methods: {
@@ -55,104 +79,140 @@ export default {
             this.size = { width: target.clientWidth, height: target.clientHeight};
         },
         initChart() {
-            let chartContainer = d3.select('#heatmap-svg')
-                .attr('viewBox', [0, 0, this.size.width, this.size.height])
+            
             const timeFormat = d3.timeFormat('%H:%M');
-            let padding = 60
-            let timeBand = tempDataErr[1].check_time - tempDataErr[0].check_time
-            let timeExtent = d3.extent(tempData.map(d => d.check_time))
+            let timeBand = temp_table_err_bf[1].check_time - temp_table_err_bf[0].check_time
+            // let timeExtent = d3.extent(temp_table_err_bf.map(d => d.check_time))
             let cpuList = ["CPU 0", "CPU 1", "CPU 2", "CPU 3"]
-
-            let xScale = d3.scaleTime()
-                    .domain([new Date(timeExtent[0].getTime() - timeBand / 1.8), new Date(timeExtent[1].getTime() + timeBand / 1.8)])
-                    .range([padding, this.size.width - padding])
-            let xTicks = d3.axisBottom(xScale)
-                .tickFormat(timeFormat)
-
-            let yScale = d3.scaleBand()
-                .domain(cpuList)
-                .range([padding, this.size.height - padding])
-                .padding(0.03)
-                // .paddingInner(0.2);
-
-            const xAxis = chartContainer.append('g')
-                .attr('transform', `translate(0, ${this.size.height - padding})`)
-                .call(xTicks)
-
-            const yAxis = chartContainer.append('g')
-                .attr('transform', `translate(${padding}, 0)`)
-                .call(d3.axisLeft(yScale))
             
-            let tempExtents = []
-            tempExtents.push(d3.extent(tempDataErr.map(d => d.cpu0)))
-            tempExtents.push(d3.extent(tempDataErr.map(d => d.cpu1)))
-            tempExtents.push(d3.extent(tempDataErr.map(d => d.cpu2)))
-            tempExtents.push(d3.extent(tempDataErr.map(d => d.cpu3)))
+            let extent = [14, 28]
             const colorBand = d3.scaleLinear()
-                .domain([d3.min(tempExtents.map(d => d[0])), d3.max(tempExtents.map(d => d[1]))])
-                .range(["white", "#69b3a2"])
+                .domain(extent)
+                .range(["white", "black"])
             
-            // heatmap blocks
-            let grid = chartContainer.append('g')
-                    
-            cpuList.forEach(item => {
-                 grid.selectAll()
-                    .data(tempDataErr)
-                    .enter()
-                    .append('rect')
-                    .attr('x', d =>  xScale(new Date(d.check_time.getTime() - timeBand / 2)))
-                    .attr('y', yScale(item))
-                    .attr('width', 0.98 * xScale(tempData[2].check_time) - xScale(tempData[0].check_time))
-                    .attr('height', yScale.bandwidth())
-                    .attr('fill', d => {
-                        if (item == 'CPU 0')
-                            return colorBand(d['cpu0'])
-                        else if (item == 'CPU 1')
-                            return colorBand(d['cpu1'])
-                        else if (item == 'CPU 2')
-                            return colorBand(d['cpu2'])
-                        else
-                            return colorBand(d['cpu3'])
-                    })
-            })
+            // tooltip event
+            let tooltip = d3.select(".tooltip-heat")
+            function mouseover(event, item) {
+                tooltip.transition()
+                    .duration(100)
+                    .style("opacity", 1);
+                console.log(d3.select("#main-container").property("offsetHeight"))
+                tooltip.html(`${item} bp ${bp_err} sb ${sb_err}`)
+                    .style("left", `${event.x}px`)
+                    .style("top", `${event.y+1000}px`)
+            }
+            function mouseout(event, d) {
+                tooltip.transition()
+                            .duration('100')
+                            .style("opacity", 0);
+            }
+            
+            function chart_init(chart_container: SVGAElement, 
+                para, bp: number, sb: number, table_data, before = true, 
+                show_cpu = true, show_time = true) {
+                let timeExtent = d3.extent(table_data.map(d => d.check_time))
+                let svgSize = para.svgSizeAf
+                if (before) {
+                    svgSize = para.svgSizeBf
+                }
                 
-            // rack grid
-        //     const grid = chartContainer.append('g')
-        //         .selectAll('rect')
-        //         .data(rack_letters.flatMap(letter => rack_nums.map(num => ({ letter, num: String(num) }))))
-        //         .enter()
-        //         .append('rect')
-        //         .attr('x', d => xScale(d.letter))
-        //         .attr('y', d => yScale(String(d.num)))
-        //         .attr('width', xScale.bandwidth())
-        //         .attr('height', yScale.bandwidth())
-        //         .attr('fill', d => (d.letter + d.num == rack_err ? '#f88379' : 'lightgray'))
-        //         .attr('stroke', 'white')
+                let xScale = d3.scaleTime()
+                    .domain([new Date(timeExtent[0].getTime() - timeBand / 1.8), new Date(timeExtent[1].getTime() + timeBand / 1.8)])
+                    .range([0, svgSize.width - para.margin.left - para.margin.right])
 
-        //     const lineStartX = xScale(rack_err[0]) + xScale.bandwidth() / 2;
-        //     const lineStartY = yScale(rack_err[1]) + yScale.bandwidth() / 2;
+                let xTicks = d3.axisBottom(xScale)
+                    .tickFormat(timeFormat)
+                    .ticks(d3.timeMinute.every(15))
+                    .tickSize(0)
 
-        //     const lineEndX = lineStartX + this.size.width / 2; 
-        //     const lineEndY = lineStartY; 
+                let yScale = d3.scaleBand()
+                    .domain(cpuList)
+                    .range([0, svgSize.height - para.margin.bottom - para.margin.top])
 
-        //     const line = chartContainer.append('line')
-        //         .attr('x1', lineStartX)
-        //         .attr('y1', lineStartY)
-        //         .attr('x2', lineStartX)
-        //         .attr('y2', lineStartY)
-        //         .attr('stroke', 'gray') 
-        //         .attr('stroke-width', 1.5)
-        //         .transition()
-        //         .duration(1000)
-        //         .attr('x2', lineEndX)
-        //         .attr('y2', lineEndY)
-        }
+                let yTicks = d3.axisLeft(yScale).tickSize(0)
+
+                if (show_time) {
+                    const xAxis = chart_container.append('g')
+                    if (!show_cpu) {
+                        xAxis.attr('transform', `translate(${0}, ${para.margin.top + svgSize.height - para.margin.bottom - para.margin.top})`)
+                    }
+                    else {
+                        xAxis.attr('transform', `translate(${para.margin.left}, ${para.margin.top + svgSize.height - para.margin.bottom - para.margin.top})`)
+                    }
+                    xAxis 
+                        .call(xTicks)
+                        .selectAll('text')
+                        .attr('transform', 'rotate(-45)') 
+                        .style('text-anchor', 'end') 
+                }
+                if (show_cpu) {
+                    const yAxis = chart_container.append('g')
+                        .attr('transform', `translate(${para.margin.left}, ${para.margin.top})`)
+                        .call(yTicks).style("stroke-width", "1px")
+                }
+                
+                let grid = chart_container.append('g')
+                    
+                if (!show_cpu) {
+                    grid.attr("transform", `translate(${0}, ${para.margin.top})`)
+                }
+                else {
+                    grid.attr("transform", `translate(${para.margin.left}, ${para.margin.top})`)
+                }
+                cpuList.forEach((item, index) => {
+                    grid.selectAll()
+                        .data(table_data)
+                        .enter()
+                        .append('rect')
+                            .attr("class", d => `t_${d.time_idx}_cpu_${index}`)
+                            .attr('x', d =>  xScale(new Date(d.check_time.getTime()) - timeBand / 2))
+                            .attr('y', yScale(item))
+                            .attr('width', 0.98 * xScale(table_data[1].check_time) - xScale(table_data[0].check_time))
+                            .attr('height', yScale.bandwidth())
+                            .attr('fill', d => {
+                                    return colorBand(d[`bp${bp}_sb${sb}_cpu_${index}_temp`])
+                            })
+                        .on('mouseover', function (event, d) {
+                            d3.selectAll('.' + this.getAttribute('class'))
+                                .transition()
+                                .duration(100)
+                                .attr('opacity', 0.85);
+                            mouseover(event, item)
+                        })
+                        .on('mouseout', function (event, d) {
+                            d3.selectAll('.' + this.getAttribute('class'))
+                                .transition()
+                                .duration('100')
+                                .attr('opacity', '1');
+                            mouseout(event, d)
+                        });
+                })
+                
+            }
+            const chartContainer = d3.select('#heatmap-svg')
+                .attr('viewBox', [0, 0, this.svgSizeBf.width, this.svgSizeBf.height])                
+            chart_init(chartContainer, this, bp_err, sb_err, temp_table_err_bf, true, true, false)
+
+            const chartContainer_af = d3.select('#heatmap-svg-af')
+                .attr('viewBox', [0, 0, this.svgSizeAf.width, this.svgSizeAf.height])
+            chart_init(chartContainer_af, this, bp_err, sb_err, temp_table_err_af, false, false, false)
+
+            const chartContainerNor_bf = d3.select('#heatmap-svg-nor')
+                .attr('viewBox', [0, 0, this.svgSizeBf.width, this.svgSizeBf.height])
+            chart_init(chartContainerNor_bf, this, bp_err, sb_err, temp_table_nor_bf, true)
+            const chartContainerNor_af = d3.select('#heatmap-svg-nor-af')
+                .attr('viewBox', [0, 0, this.svgSizeAf.width, this.svgSizeAf.height])
+            chart_init(chartContainerNor_af, this, bp_err, sb_err, temp_table_nor_af, false, false, true)
             
+    
+                
+        }
     },
     watch: {
         size(newSize) {
             if (newSize.width > 0 && newSize.height > 0) {
                 d3.select('#heatmap-svg').selectAll('*').remove()
+                // d3.select('#heatmap-svg-nor').selectAll('*').remove()
                 this.initChart()
             }
         }
@@ -167,15 +227,51 @@ export default {
 }
 </script>
 <template>
-    <div ref="heatmapContainer" class="chartContainer">
-        <svg id="heatmap-svg">
-        </svg>
+    <div ref="heatmapContainer" class="heatmapContainer">
+        <!-- <h2>Heatmap Layout</h2> -->
+        <v-row>
+            <h3 id = "paragraph-bf">Before the error occurs</h3> 
+            <h3 id = "paragraph-af">After the error occurs</h3> 
+
+        </v-row>
+        <v-col id = "root">
+            <svg id="heatmap-svg" class = "svg-container-bf"></svg>
+            <svg id="heatmap-svg-af" class = "svg-container-af"></svg>
+            
+        </v-col>
+        <v-col>
+            <!-- <svg id="heatmap-svg"></svg> -->
+            <svg id="heatmap-svg-nor" class = "svg-container-bf"></svg>
+            <svg id="heatmap-svg-nor-af" class = "svg-container-af"></svg>
+        </v-col>
+        <div class = "tooltip-heat">
+        </div>        
     </div>
 </template>
 
 <style scoped>
-.chartContainer {
-    height: 500px;
-    width: 1000px;
+.heatmapContainer {
+    height: 1000px;
+    width: 1050px;
+}
+.svg-container-bf {
+    height: 102px;
+    width: 255px; 
+}
+.svg-container-af {
+    height: 102px;
+    width: 730px; 
+}
+.tooltip-heat{
+    position: absolute;
+    line-height: 1;
+    font-weight: bold;
+    padding: 10px;
+    background: rgba(0, 0, 0, 0.8);
+    color: #fff;
+    border-radius: 2px;
+    font-size: 12px;
+    text-align: left;
+    opacity: 0;
 }
 </style>
